d5b519303171da07a04baae21dae2c1327c50e93
Siddhesh172004 <84581657+Siddhesh172004@users.noreply.github.com>
2021-07-06 02:44:24 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,47 @@
+const input=document.querySelector('#add-text');
+const addbtn=document.querySelector('#add');
+
+addbtn.addEventListener('click',addtask);
+
+input.addEventListener('keyup', (e)=>{
+    (e.keyCode === 13 ? addtask(e) : null);
+})
+
+
+
+    function addtask(e){
+        const notcompleted=document.querySelector('.not-completed');
+        const completed=document.querySelector('.completed');
+
+        const inputli=document.createElement('li');
+        const checkbtn=document.createElement('button');
+        const trashbtn=document.createElement('button');
+      
+			checkbtn.innerHTML = '<i class="fa fa-check"></i>';
+			trashbtn.innerHTML = '<i class="fa fa-trash"></i>';
+
+        if(input.value !==''){
+            inputli.textContent=input.value;
+            input.value='';
+            notcompleted.appendChild(inputli);
+            inputli.appendChild(checkbtn);
+            inputli.appendChild(trashbtn);
+
+        }
+
+        /* removinig from not completed*/
+        checkbtn.addEventListener("click",function(){
+            const removed=this.parentNode;
+                removed.remove();
+        completed.appendChild(inputli);
+        checkbtn.style.display="none";
+        });
+
+          /* removinig from  completed*/
+          trashbtn.addEventListener("click",function(){
+            const removed=this.parentNode;
+                removed.remove();
+      
+        });
+
+    }
